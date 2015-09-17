c19f1fed908a3cd94ee7960fba38a95b59362927
Sam Wolfson <terabyte128@gmail.com>
2015-09-16 21:10:41 -0700

    Initial Commit

---
+++ BogoSortC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BogoSortC
+//
+//  Created by Sam Wolfson on 9/16/15.
+//  Copyright (c) 2015 Sam Wolfson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
